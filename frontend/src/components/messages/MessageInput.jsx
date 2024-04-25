5d717d6028a38d1735e10be69415d20c4d80cc17
pratyush <pratyush.epi.spn117@gmail.com>
2024-04-25 23:35:53 +0530

    2nd part completed- ui design

---
+++ frontend/src/components/messages/MessageInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsSend} from "react-icons/bs"
+const MessageInput = () => {
+  return (
+   <form action="">
+    <div className="w-full relative">
+        <input type="text"
+        className='border text-sm rounded-lg block w-full p-2.5 bg-gray700 border-gray-600 text-white'
+        placeholder='Send a message' />
+        <button className="absolute inset-y-0 end-0 flex items-center pe-3">
+            <BsSend/>
+        </button>
+    </div>
+   </form>
+  )
+}
+
+export default MessageInput
