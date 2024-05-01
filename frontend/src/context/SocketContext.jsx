b8bbb24529311f235740f43a1b260dd493a7779c
pratyush <pratyush.epi.spn117@gmail.com>
2024-05-01 22:14:03 +0530

    Socket fix

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:5000",{
+            const socket=io("https://pratyush-chatapp.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
